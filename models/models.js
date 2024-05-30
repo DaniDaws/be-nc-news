fefa59336aebde36ce03d64680dc5449191b3658
Danielle Dawson <danielle.daisy.97@gmail.com>
2024-05-30 15:11:25 +0100

    modifies the model selectArticleById to use the aggregate function count() to return comment_count, adds one test to the test suite for /api/articles/:article_id, and adds comment_count to the exampleResponse in endpoints.json

--- models/models.js
+++ models/models.js
@@ -7,9 +7,12 @@ exports.fetchTopics = () => {
 exports.selectArticleById = (article_id) => {
   return db
     .query(
-      `SELECT author, title, article_id, body, topic, created_at, votes, article_img_url
-         FROM articles
-         WHERE article_id = $1`,
+      `SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
+              COUNT(comments.comment_id) AS comment_count
+       FROM articles
+       LEFT JOIN comments ON comments.article_id = articles.article_id
+       WHERE articles.article_id = $1
+       GROUP BY articles.article_id;`,
       [article_id]
     )
     .then(({ rows }) => {
