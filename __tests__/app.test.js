885078cd44385309c4bc3bd5c543fd02291a30f8
Danielle Dawson <danielle.daisy.97@gmail.com>
2024-05-30 19:18:35 +0100

    refactors two tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -88,7 +88,19 @@ describe("/api/articles/:article_id", () => {
           created_at: expect.any(String),
           votes: expect.any(Number),
           article_img_url: expect.any(String),
-          comment_count: expect.any(String),
+          comment_count: expect.any(Number),
+        });
+        expect(article).toEqual({
+          author: "butter_bridge",
+          title: "Living in the shadow of a great man",
+          article_id: 1,
+          body: "I find this existence challenging",
+          topic: "mitch",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: 11,
         });
       });
   });
--- models/models.js
+++ models/models.js
@@ -8,7 +8,7 @@ exports.selectArticleById = (article_id) => {
   return db
     .query(
       `SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
-              COUNT(comments.comment_id) AS comment_count
+              COUNT(comments.comment_id) ::INT AS comment_count
        FROM articles
        LEFT JOIN comments ON comments.article_id = articles.article_id
        WHERE articles.article_id = $1
