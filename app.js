70c52cdcd9609c64948bbc5c3be3c7874a18df50
Danielle Dawson <danielle.daisy.97@gmail.com>
2024-06-17 10:59:28 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const {
   getTopics,
   getEndpoints,
@@ -11,6 +12,8 @@ const {
   getUsers,
 } = require("./controllers/controllers");
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
